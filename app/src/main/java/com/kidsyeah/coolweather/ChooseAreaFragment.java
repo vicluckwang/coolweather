4b92694b50f69a27fbcf4d3cd25bb7ea6b75e47f
wangvic <wangvic@wangvicdeiMac.local>
2018-10-20 15:36:10 +0800

    新增切换城市和下拉刷新天气的功能

--- app/src/main/java/com/kidsyeah/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/kidsyeah/coolweather/ChooseAreaFragment.java
@@ -117,10 +117,19 @@ public class ChooseAreaFragment extends Fragment {
                     } else {
                         location = countyName;
                     }
-                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
-                    intent.putExtra("location", location);
-                    startActivity(intent);
-                    getActivity().finish();
+
+                    if(getActivity() instanceof MainActivity) {
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("location", location);
+                        startActivity(intent);
+                        getActivity().finish();
+                    } else if(getActivity() instanceof WeatherActivity) {
+                        WeatherActivity activity = (WeatherActivity)getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefreshLayout.setRefreshing(true);
+                        activity.requestWeather(location);
+                    }
+
                 }
             }
         });
--- app/src/main/java/com/kidsyeah/coolweather/WeatherActivity.java
+++ app/src/main/java/com/kidsyeah/coolweather/WeatherActivity.java
@@ -5,10 +5,14 @@ import android.graphics.Color;
 import android.os.Build;
 import android.preference.Preference;
 import android.preference.PreferenceManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -29,6 +33,12 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class WeatherActivity extends AppCompatActivity {
+
+    public DrawerLayout drawerLayout;
+    private Button navButton;
+
+    public SwipeRefreshLayout swipeRefreshLayout;
+    private String mLocation;
 
     private ScrollView weatherLayout;
     private TextView titleCity;
@@ -68,18 +78,39 @@ public class WeatherActivity extends AppCompatActivity {
 
         bingPicImg = (ImageView) findViewById(R.id.bing_pic_img);
 
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navButton = (Button) findViewById(R.id.nav_button);
+
+        navButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        swipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.swipe_refresh);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        String weatherString = null; //prefs.getString("weather", null);
+        String weatherString = prefs.getString("weather", null);
         if(weatherString != null) {
             //有缓存时直接解析天气数据
             Weather weather = Utility.handleWeatherResponse(weatherString);
+            mLocation = weather.basic.location;
             showWeatherInfo(weather);
         } else {
             //无缓存时去服务器查询天气
-            String location = getIntent().getStringExtra("location");
+            mLocation = getIntent().getStringExtra("location");
             weatherLayout.setVisibility(View.INVISIBLE);
-            requestWeather(location);
+            requestWeather(mLocation);
         }
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                requestWeather(mLocation);
+            }
+        });
 
         String bingPic = prefs.getString("bing_pic", null);
         if(bingPic != null) {
@@ -103,10 +134,12 @@ public class WeatherActivity extends AppCompatActivity {
                             SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
                             editor.putString("weather", responseText);
                             editor.apply();
+                            mLocation = weather.basic.location;
                             showWeatherInfo(weather);
                         } else {
                             Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
                         }
+                        swipeRefreshLayout.setRefreshing(false);
                     }
                 });
             }
